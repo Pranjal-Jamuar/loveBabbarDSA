dd84530ebf91066925d148c641ca3558d2fb3e45
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-06-27 10:13:34 +0530

    added new file

---
+++ 1.conditionalsAndLoops/ifElseStatements/checkNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num;
+
+  cout << "Enter the number:";
+  cin >> num;
+
+  if (num >= 0)
+  {
+    cout << "The number entered is positive.";
+  }
+  else if (num <= 0)
+  {
+    cout << "The number entered is negative.";
+  }
+  else if (num == 0)
+  {
+    cout << "The number entered is zero.";
+  }
+
+  return 0;
+}
