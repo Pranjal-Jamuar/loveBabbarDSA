38b907e6f27027c5c541a4a50b0ff441224df76f
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-06-30 09:23:56 +0530

    new file added

---
+++ 1.conditionalsAndLoops/forLoop/halfPyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int rows;
+  cout << "Enter the number of rows:";
+  cin >> rows;
+
+  for (int i = 0; i < rows; i = i + 1)
+  {
+    for (int j = 0; j < i + 1; j = j + 1)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
