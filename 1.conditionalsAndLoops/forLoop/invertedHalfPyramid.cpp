8270eb2446428c7e47e7c705eed8359a3b580cbb
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-06-30 09:35:50 +0530

    did another pattern

--- 1.conditionalsAndLoops/forLoop/invertedHalfPyramid.cpp
+++ 1.conditionalsAndLoops/forLoop/invertedHalfPyramid.cpp
@@ -2,3 +2,19 @@
 using namespace std;
 
 int main()
+{
+  int rows;
+  cout << "Enter the number of rows:";
+  cin >> rows;
+
+  for (int i = 0; i < rows; i = i + 1)
+  {
+    for (int j = 0; j < rows - i; j = j + 1)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
