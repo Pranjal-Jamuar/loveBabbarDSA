2fbcf1be64d6cca69791016ed8426772656dc402
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-06-30 09:52:40 +0530

    updated code

--- 1.conditionalsAndLoops/forLoop/numericHalfPyramid.cpp
+++ 1.conditionalsAndLoops/forLoop/numericHalfPyramid.cpp
@@ -7,11 +7,11 @@ int main()
   cout << "Enter the number of rows:";
   cin >> rows;
 
-  for (int i = 1; i <= rows; i++)
+  for (int i = 0; i < rows; i++)
   {
-    for (int j = 1; j < i + 1; j++)
+    for (int j = 0; j < i + 1; j++)
     {
-      cout << j << " ";
+      cout << j + 1 << " ";
     }
     cout << endl;
   }
